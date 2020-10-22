67fdbb606b206eb0ad182b31a0d372ded34b7a84
federpmatc <61851250+federpmatc@users.noreply.github.com>
2020-10-22 12:48:54 -0500

    Update chp 7.ps1

--- chp 7.ps1
+++ chp 7.ps1
@@ -1,11 +1,20 @@
-﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  #Locally created scripts will run
+#Chapter 7 - Adding Commands
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  #Locally created scripts will run
+
+#profiles
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-5.1
 
 #https://www.howtogeek.com/50236/customizing-your-powershell-profile/
 #There are multiple profiles
+
+#A PowerShell profile is a script that runs when PowerShell starts. 
+#You can use the profile as a logon script to customize the environment. 
+
 $profile
 $profile | select *
 
-$PROFILE.CurrentUserCurrentHost #Current User Current Host  (console or ISE)
+$PROFILE.CurrentUserCurrentHost #Current User Current Host  (the host can be console or ISE)
 $PROFILE.AllUsersCurrentHost #All users Current Host
 $PROFILE.CurrentUserAllHosts 
 $PROFILE.AllUsersAllHosts
@@ -24,8 +33,8 @@ Add-Content -Value "function h10 { Get-History -Count 10 }" -Path $profile
 notepad $profile
 
 #add the following lines to the script
-new-item alias:np -value notepad.exe
-$Shell = $Host.UI.RawUI   #$Host is a variable that exposes
+New-Alias -Name NP -Value notepad.exe
+$Shell = $Host.UI.RawUI   #$Host is a variable that exposes PS environment
 
 #PowerShell provides a built-in variable for accessing details about the PowerShell environment (Get-Host object.) 
 $shell.BackgroundColor = "black"
@@ -40,21 +49,35 @@ Remove-Item $profile
 #modules are a way to extend PowerShell
 Get-ChildItem env:
 Get-ChildItem Env:\PSModulePath #PowerShell automatically looks for modules here
-$env:PSModulePath -split ';'
+$env:PSModulePath -split ';'    #Split is a PowewrShell operator that splits strings
 
 Get-Module
 Get-Module -ListAvailable
 
+#We need to install Nuget, which is a package provider used by the PS Repository
+#Refer to https://docs.microsoft.com/en-us/nuget/what-is-nuget for more info on NuGet
+
+Get-PackageProvider -ListAvailable #get list of all package providers that are available on the local computer.
+Find-PackageProvider -Name "Nuget" -AllVersions
+#We need to install NuGet
+#The Microsoft-supported mechanism for sharing code is NuGet, which defines how packages for .NET are created, hosted, and consumed
+
+[Net.ServicePointManager]::SecurityProtocol
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+[Net.ServicePointManager]::SecurityProtocol
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Get-PackageProvider #Get all currently loaded package providers
+
 #Getting modules from the internet
-#http://powershellgallery.com
-Get-PSRepository
+#http://powershellgallery.com (could use find-module ntfs*)
+Install-Module -Name NTFSSecurity
 
-Find-Module *ntfs* #From PS Repository
-install-module NTFSSecurity #Download & install module
-
-GCM -Module NTFSSecurity #Checkout the commands
+Get-Command -Module NTFSSecurity #Checkout the commands
 Import-Module NTFSSecurity #Load the module
 
 Get-InstalledModule
 Get-Module
-Get-Module -ListAvailable
+
+Get-Module -ListAvailable 
