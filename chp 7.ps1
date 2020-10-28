541ef0890a50a8b233b7654fafa2bad74e1f7994
federpmatc <61851250+federpmatc@users.noreply.github.com>
2020-10-28 10:25:44 -0500

    Update chp 7.ps1

--- chp 7.ps1
+++ chp 7.ps1
@@ -1,15 +1,11 @@
 #Chapter 7 - Adding Commands
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  #Locally created scripts will run
-
-#profiles
+#A PowerShell profile is a script that runs when PowerShell starts
 #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-5.1
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  #Locally created scripts will run
 
 #https://www.howtogeek.com/50236/customizing-your-powershell-profile/
 #There are multiple profiles
-
-#A PowerShell profile is a script that runs when PowerShell starts. 
-#You can use the profile as a logon script to customize the environment. 
 
 $profile
 $profile | select *
@@ -24,7 +20,7 @@ $profile
 Test-Path $profile
 
 #The best one to use
-New-Item -path $profile -type file –force
+New-Item -path $profile -type file -Force
 Add-Content -Path $profile '$Shell = $Host.UI.RawUI'
 Add-Content -Path $profile '$Shell.WindowTitle="Pat Feder - PowerShell"'
 Add-Content -Value "function h50 { Get-History -Count 50 }" -Path $profile
@@ -54,30 +50,28 @@ $env:PSModulePath -split ';'    #Split is a PowewrShell operator that splits str
 Get-Module
 Get-Module -ListAvailable
 
-#We need to install Nuget, which is a package provider used by the PS Repository
-#Refer to https://docs.microsoft.com/en-us/nuget/what-is-nuget for more info on NuGet
+#https://gallery.technet.microsoft.com/scriptcenter/How-to-switch-UAC-level-0ac3ea11
 
-Get-PackageProvider -ListAvailable #get list of all package providers that are available on the local computer.
-Find-PackageProvider -Name "Nuget" -AllVersions
-#We need to install NuGet
+
+Get-Package #installed s/w packages
+#Package Management - https://www.sconstantinou.com/powershell-packagemanagement/
+#Regardless of the installation technology underneath, users can use these common cmdlets to install/uninstall 
+#packages, add/remove/query package repositories, and query a system for the software installed.
+
+Get-PackageProvider #providers (technologies) used to install our software packages
+
+#PowerShellGet is a module for locating and installing scripts, modules,etc.
+Get-Command -Module PowerShellGet
+
+Get-PSRepository    #Registered package repositories containing scripts, modules, etc
+#Use the cmdlets in the PowerShellGet module to install packages from the PowerShell Gallery
+
+Find-Module *ntfs*    #locate modules in our PS Repository
+Install-Module NTFSSecurity #install modules
+#Nuget was installed, which is a package provider now used by the PS Repository
 #The Microsoft-supported mechanism for sharing code is NuGet, which defines how packages for .NET are created, hosted, and consumed
-
-[Net.ServicePointManager]::SecurityProtocol
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-[Net.ServicePointManager]::SecurityProtocol
-
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Get-PackageProvider #Get all currently loaded package providers
-
-#Getting modules from the internet
-#http://powershellgallery.com (could use find-module ntfs*)
-Install-Module -Name NTFSSecurity
+#Refer to https://docs.microsoft.com/en-us/nuget/what-is-nuget
 
 Get-Command -Module NTFSSecurity #Checkout the commands
-Import-Module NTFSSecurity #Load the module
 
 Get-InstalledModule
-Get-Module
-
-Get-Module -ListAvailable 
