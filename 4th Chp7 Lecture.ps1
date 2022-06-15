257af6c350eb1d12e45bd3f2f7d3864b99cabd9d
federpmatc <61851250+federpmatc@users.noreply.github.com>
2022-06-15 10:09:16 -0500

    Update 4th Chp7 Lecture.ps1

--- 4th Chp7 Lecture.ps1
+++ 4th Chp7 Lecture.ps1
@@ -1,8 +1,8 @@
 #Chapter 7 - Adding Commands
 
-
 #modules are a way to extend PowerShell
 Get-ChildItem env:
+
 Get-ChildItem Env:\PSModulePath #PowerShell automatically looks for modules here
 $env:PSModulePath -split ';'    #Split is a PowewrShell operator that splits strings
 
@@ -14,10 +14,19 @@ Get-Command -Module PowerShellGet
 
 Get-PSRepository    #Registered package repositories containing scripts, modules, etc
 #Use the cmdlets in the PowerShellGet module to install packages from the PowerShell Gallery
+#This is untrusted, because it's possible for people to load malicious code
 
 Find-Module *ntfs*    #locate modules in our PS Repository
+Save-Module NTFSSecurity
+
 Install-Module NTFSSecurity #install modules
 
 Get-Command -Module NTFSSecurity #Checkout the commands
 
 Get-InstalledModule
+
+Get-NTFSAccess C:\Share
+Get-Item C:\Share | Get-NTFSAccess
+Get-Item C:\Share | Disable-NTFSInheritance
+Get-Item C:\Share | Get-NTFSAccess -ExcludeInherited | Export-Csv permissions.csv
+Import-Csv C:\Share | Add-NTFSAccess
