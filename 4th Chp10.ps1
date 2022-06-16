dc14c484d4f374f11d9852eee5c376a4e3289617
federpmatc <61851250+federpmatc@users.noreply.github.com>
2022-06-16 15:24:43 -0500

    Update 4th Chp10.ps1

--- 4th Chp10.ps1
+++ 4th Chp10.ps1
@@ -2,13 +2,22 @@
 Get-Hotfix -computerName (get-adcomputer -filter {name -like '*Server*'} | Select-Object -expand name) 
 #Q2
 get-adcomputer -filter {name -like '*Server*' } | Get-Hotfix
+
+#Pipe Input by property name
+get-adcomputer -filter {name -like '*Server*' } | select-object *, @{n='computername';e={$_.name}} |Get-Hotfix
+
+#Pipe Input by Value (object type)
+get-adcomputer -filter {name -like '*Server*' } | select-object * -ExpandProperty name  |Get-Hotfix
+
 #Q3
-get-adcomputer -filter  {name -like 'Server2019*' }  | foreach {Get-HotFix -ComputerName $_.name}
+get-adcomputer -filter  {name -like 'Server2019*' }  | ForEach-Object {Get-HotFix -ComputerName $_.name}
+
 #Q4
 $names = get-adcomputer -filter  {name -like 'Server2019*' } | Select-Object -expandproperty name
 Get-HotFix -ComputerName $names
+
 #Q5
-Get-Process -computername (get-adcomputer -filter {name -like '*Server*'} | Select-Object -expand name)
+Test-Connection -computername (get-adcomputer -filter {name -like '*Server*'} | Select-Object -expand name)
 
 #region Q6
 #I found the following online to delete all users with a name property that begins with 'User'
@@ -16,17 +25,17 @@ Get-Process -computername (get-adcomputer -filter {name -like '*Server*'} | Sele
 get-aduser -Filter "name -like 'user*'" | Remove-ADUser -Confirm:$false
 
 #A .csv file was created as described in Chapter 9, question 6
-Get-content .\Chp9Q6.csv
+Get-content .\Chp10Q6.csv
 
 #this will output the contents of the .csv as 3 objects
 #each object contains the folloing properties - name, company, city, title
-Import-Csv .\Chp9Q6.csv
+Import-Csv .\Chp10Q6.csv
 
 #The following 3 lines are a single command.  The last character
 #on each line is a '|' so PowerShell knows that the command continues
 #on the following line
 #The following creates three users 
-Import-Csv C:\users\Administrator\Chp9Q6.csv |
+Import-Csv C:\users\Admin\Chp10Q6.csv |
 Select-Object name, company, city, title, @{n='samAccountName'; e={$_.Name}}|
 New-ADUser
 
@@ -37,9 +46,7 @@ get-aduser -Filter "name -like 'user*'"
 get-aduser -Filter "name -like 'user*'" | Set-ADAccountPassword -Reset `
 -NewPassword (ConvertTo-SecureString -AsPlainText 'Pa$$w0rd' -Force) `
 -PassThru | Enable-ADAccount
-#endregion Q6
+#endregion
 
 #Q6 - FWIW
- get-aduser -Filter "name -like 'user*'" | Set-ADAccountPassword -Reset `
--NewPassword (ConvertTo-SecureString -AsPlainText 'Pa$$w0rd' -Force) `
--PassThru | Enable-ADAccount 
+ get-aduser -Filter "name -like 'user*'" | Remove-ADUser
