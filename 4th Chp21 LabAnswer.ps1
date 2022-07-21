fa2a925282d5013b281ce55648bb900bf780caa4
federpmatc <61851250+federpmatc@users.noreply.github.com>
2022-07-21 13:39:25 -0500

    Update 4th Chp21 LabAnswer.ps1

--- 4th Chp21 LabAnswer.ps1
+++ 4th Chp21 LabAnswer.ps1
@@ -1,20 +1,26 @@
-#1
-Get-ChildItem c:\windows | where {$_.name -match "\d{2}"}
-Get-ChildItem /usr | where {$_.name -match "\d{2}"}
+#1 
+Get-ChildItem c:\windows | Where-Object {$_.name -match "\d{2}"}
+Get-ChildItem /usr | where-object {$_.name -match "\d{2}"}
 
 #2 
-get-module | where {$_.companyname -match "^Microsoft"} | Select Name,Version,Author,Company
+get-module | where-object {$_.companyname -match "^M.crosoft"} | Select-Object Name,Version,Author,Company
 
 #3 
-get-content C:\Windows\WindowsUpdate.log |Select-string "[\w+\W+]Installing Update"
+Get-Content C:\Windows\Logs\CBS\CBS.log |  Select-string "[\w+\W+] Update" | Out-GridView
+Get-Content C:\Windows\Logs\CBS\CBS.log |  Where-Object {$_ -match "[\w+\W+] Update" } | Out-GridView
+
 Get-content ./apt/history.log | select-string "[\w+\W+]Installing"
 
 #4 
 #You could get by with a pattern that starts with one to three numbers followed by a literal period, like this:
-get-dnsclientcache | where { $_.data -match "^\d{1,3}\."}
+get-dnsclientcache | where-object { $_.data -match "^\d{1,3}\."}
 
 #Or you could match an entire IPv4 address string:
-get-dnsclientcache | where{ $_.data -match "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"}
+get-dnsclientcache | where-object{ $_.data -match "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"}
 
 #5 
-gc /etc/hosts | where {$_ -match "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"}
+#modified for Windows.  Also removed changed search to not start at first char
+Get-Content C:\Windows\System32\drivers\etc\hosts | where-object {$_ -match "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"}  
+
+Get-Content C:\Windows\System32\drivers\etc\hosts | Select-String "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" | Out-GridView
+
