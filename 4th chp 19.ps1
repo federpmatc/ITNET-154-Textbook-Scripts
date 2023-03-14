e00dca049b4967763066e58c4a3cff80b3dabedd
federpmatc <61851250+federpmatc@users.noreply.github.com>
2023-03-14 12:16:17 -0500

    Update 4th chp 19.ps1

--- 4th chp 19.ps1
+++ 4th chp 19.ps1
@@ -26,3 +26,41 @@ Invoke-Command -ComputerName $computername -ScriptBlock {
     
     "$env:COMPUTERNAME Total Free Space $([int]($Count / 1gb))GB"
 }
+
+
+#Added Help and support for arrays
+<#
+.SYNOPSIS
+Get-DiskInventory retrieves logical disk information from one or
+more computers.
+.DESCRIPTION
+Get-DiskInventory uses Get-PSDrive
+.PARAMETER computername
+The computer name, or names, to query. Default: Server2019-1
+.PARAMETER name
+Just display additional text to include in the output
+Using parameter
+.EXAMPLE
+Get-DiskInventory -computername SERVER-R2 -name Pat
+.EXAMPLE
+Get-DiskInventory W10-CLient Pat
+#>
+
+<#
+blah
+blah
+blah
+#>
+param (
+    [string[]] $computername = @("Server2019-1","W10-Client"),
+    [string] $name = "Pat"
+)
+
+#"....................$(Get-Date) Hey $name Checking the disk space on $computername  ..............................."
+Invoke-Command -ComputerName $computername -ScriptBlock {
+    Get-PSDrive | ?{$_.Free -gt 1} | 
+    %{$Count = 0; Write-Host "";} `
+    { $_.Name + ": Used: " + "{0:N2}" -f ($_.Used/1gb) + " Free: " + "{0:N2}" -f ($_.free/1gb) + " Total: " + "{0:N2}" -f (($_.Used/1gb)+($_.Free/1gb)); $Count = $Count + $_.Free;}`
+    
+    "$env:COMPUTERNAME Total Free Space $([int]($Count / 1gb))GB"
+}
