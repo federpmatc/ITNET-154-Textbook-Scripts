d5ca8aa7f24b26b584ccb764763a31d982462d30
federpmatc <61851250+federpmatc@users.noreply.github.com>
2024-01-24 11:51:30 -0600

    Update chp 3 and 4.ps1
    Align closer to textbook

--- chp 3 and 4.ps1
+++ chp 3 and 4.ps1
@@ -1,32 +1,33 @@
 #Chapter 3
 
 #The first time you try to get help on a new system..nothing
-get-help get-service -Examples
+get-help Get-Item -Examples
 
 #Update-Help updates the help for all installed modules
 #in a location listed in the $env:PSModulePath environment variable
 
 update-help  #as user
-update-help  #as admin
+update-help  #as admin (required with Windows PowerShell)
 
-get-help get-service -Examples
-get-help get-service  -Full
-get-help get-service -online #
+get-help -name get-item -Examples
+get-help get-item -Examples #positional parameter and a switch
+get-item . #get the current directory
+get-item * #get the contents
+get-item -Path ~\*
 
-help Get-Disk -Examples
+help get-item  -Full
+help get-item -online #
 
 #I prefer just googling powershell get-service
 #help & get-help are basically the same, help is a function that pipes the output of get-help to more
 
-help *service* #talk about wild cards
+help *item* #talk about wild cards
 
-help Get-EventLog -Full
 
 # We see that Get-Eventlog actually outputs 2 different types of info.
 Get-EventLog -AsString #-AsString is a switch.
 Get-EventLog -logname system 
 Get-EventLog  system #-logname is a positional parameter
-
 Get-EventLog -logname system -Newest 20 -InstanceId 20003 #-newest is an optional paramter
 
 #The following requires that Remote Registry service be started & Firewall off & PowerShell run as admin
@@ -36,37 +37,32 @@ Get-EventLog -logname system -Newest 20 -InstanceId 20003 #-newest is an optiona
 Get-EventLog -ComputerName Server2019-1 -LogName System -Newest 5
 #endregion 
 
+help new-item
+New-Item -path "~\servers.txt"  -ItemType file  -force #
+add-content -Path "~\servers.txt" -Value "Server2019-1","Server2019-2", "W10-Client"
+get-content ~\servers.txt
 
-New-Item -path "c:\servers.txt"  -ItemType file # -Value "Server2019-1`r`nServer2019-2`r`nLocalhost"
-add-content -Path "c:\servers.txt" -Value "Server2019-1","Server2019-2", "W10-Client"
-get-content C:\servers.txt
-notepad C:\servers.txt 
+help about*   #background topics
+help about_Aliases
+New-Alias -Name ghh -Value Get-Help
 
-Get-Content "C:\servers.txt"
-
-#The -Computername paramter uses RPC, Remote Registry Service Running and requires Firewall turned off or at least port 445 open
-#New-NetFirewallRule -DisplayName Open445 -Direction Inbound -Action Allow -Protocol tcp -LocalPort 445 
-#Set-Service -Name RemoteRegistry -StartupType Manual
-#Set-Service -Name RemoteRegistry -status Running
-#Set-NetFirewallProfile -All -Enabled False
-
-Get-EventLog -LogName System -ComputerName server2019-2 -Newest 3
-Get-EventLog -LogName System -ComputerName server2019-2,Server2019-1,W10-Client -Newest 3
-Get-EventLog -LogName System -ComputerName (Get-Content C:\servers.txt) -Newest 1
-
-#Chapter 4
+#Chapter 4 Running Commands
 #####################################################################################
-#get-command is designed to retrieve only commands
+$diskInfo = 'Get-PSDrive | ?{$_.Free -gt 1} | %{$Count = 0; Write-Host "";} { $_.Name + ": Used: " + "{0:N2}" -f ($_.Used/1gb) + " Free: " + "{0:N2}" -f ($_.free/1gb) + " Total: " + "{0:N2}" -f (($_.Used/1gb)+($_.Free/1gb)); $Count = $Count + $_.Free;}{Write-Host"";Write-Host "Total Free Space " ("{0:N2}" -f ($Count/1gb)) -backgroundcolor magenta}'
+New-Item -path "~\DiskInfo.ps1"  -ItemType file  -force #
+add-content -Path "~\DiskInfo.ps1"  -Value $diskInfo
+Get-Content -Path "~\DiskInfo.ps1"
 
-get-command get-se*
-get-command -verb get
-gcm -Noun service
+Get-ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  #Run local scripts or downloaded scripts that are digitally signed
+Set-ExecutionPolicy -ExecutionPolicy Restricted  #Scripts aren't executed
+
 
 #4.3 cmdlet, function, application
-#cmdlet is a PowerShell Command
+#cmdlet is a PowerShell Command (verb-noun)
 #Function are written in PowerShell
-#Application is an external command
-#Alias is like a shortcut
+#Application is an external command (like ping)
+#Alias is like a shortcut (gci)
 
 #function
 Function Get-MyName
@@ -79,8 +75,8 @@ Get-MyName
 
 Function Get-NewFiles
 {
- $Start = (Get-Date).AddMonths(-1)
- get-childitem -Path c:\*.* | Where-Object LastWriteTime -gt $Start
+ $Start = (Get-Date).AddMonths(-6)
+ get-childitem -Path ~ | Where-Object LastWriteTime -gt $Start
 }
 
 get-newfiles
@@ -93,19 +89,30 @@ get-command -commandtype cmdlet
 Get-Alias
 gal s*
 gal -Definition get*
+gal -Definition *item*
 get-alias -definition get-command
 get-alias -Name g*
 
 #############
-gcm *copy*
+get-command *copy*
 help Copy-Item
 #Look at the examples how do I copy one directory (and it's contents) to another directory?
 
-Get-Item C:\windows\*
-Get-Item C:\windows\ADFS
+Get-Item ~\Documents\*
 
 #region Challenge
-New-Item c:\temp -ItemType directory
-Copy-Item C:\Windows\ADFS -Destination C:\temp -Recurse
-Get-ChildItem C:\temp -Recurse
+New-Item ~\temp -ItemType directory
+Copy-Item ~\Documents\* -Destination ~\temp # -Recurse
+Get-ChildItem ~\temp #-Recurse
 #endregion
+
+#rewrite the above with aliases
+
+#Support External Commands
+ping -c 20 8.8.8.8
+
+$exe = "ping"
+$count = 5
+$address = "8.8.8.8"
+
+& $exe -c $count $address   #Invocation Operator, variables, external commands
