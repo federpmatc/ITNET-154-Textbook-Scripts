1516e3735840404a1ff6b0073cb0359238c8ba83
federpmatc <61851250+federpmatc@users.noreply.github.com>
2024-01-29 09:42:40 -0600

    Update 4th chp 5 chp 6.ps1

--- 4th-chp5.ps1
+++ 4th-chp5.ps1
@@ -1,23 +1,29 @@
-#Q1
-New-Item -ItemType Directory -Name labs
+Get-PSDrive
 
+#CmdLets to see and manipulate the data in a PSDrive include an ‘item’ noun)
+Get-Item Alias:
+Get-Item /   #refers to the object
+#On macOS and Linux we don't have drive letters.  The entire FS is mapped to  /
+Get-ChildItem / #refers to the items within the object
+Get-ChildItem / #Let's see what is in it
+
+#Most items have properties, just like files and folders have properties
+Get-ItemProperty /Users  #Items have properties
+Get-ItemProperty /Users  | select *
+
+New-Item -ItemType Directory -Name labs
 New-item -ItemType File -Name labs\test.txt
 
-Set-Item -Path .\labs\test.txt -Value "hello"
+Set-Item -Path labs\test.txt -Value "hello"  #not supported on mac
+Add-Content -Path  ./labs/test.txt -Value "hello"
 
-Get-Item Env:Path
-Dir Env:Path 
-Gci Env:Path
+Get-Item Env:/PSModulePath
+Get-Item Env:/PSModulePath | select *
 
-gci -Path Desktop -Filter *.txt
-gci -Path Desktop -Exclude *.txt
-Get-ChildItem -Path C:\Users\Admin.ITNET\Desktop\* -Include *.rtf
+Get-ChildItem -Path ~/Downloads -Filter *.txt #seems to be the best
+Get-ChildItem -Path ~/Downloads/* -Include *.txt,*.pdf  #include supports multiple conditions
+Get-ChildItem -Path ~/Downloads/* -Exclude *.txt -Filter D*
 
 Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 get-item -Path  Advanced
 Set-ItemProperty -path Advanced -PSProperty DontPrettyPath -Value 1
-
-Set-Location C:\Users\Admin.ITNET\labs
-Add-Content -Path .\test.txt -Value "Hello" 
-
-notepad .\test.txt
