79a46d90b0ae99b50abd88acd14e9b3d96c41835
federpmatc <61851250+federpmatc@users.noreply.github.com>
2024-02-27 09:35:18 -0600

    Update Chapter 12 Lecture (4th).ps1

--- Chapter 12 Lecture (4th).ps1
+++ Chapter 12 Lecture (4th).ps1
@@ -1,4 +1,4 @@
-﻿#Comparison Operators & Filtering
+#Comparison Operators & Filtering
 #Comparison operators can be used to filter objects out of the pipeline
 #Comparison involve 2 objects, values and testing their relationship
 
@@ -7,6 +7,8 @@
 5 -eq 5
 5 -ne 5
 
+#In PowerShell, the logical operators are used to connect statements or expressions to form a single expression. 
+#The expressions which contain the logical operators usually result in the Boolean values $True or $False.
 (5 -lt 6) -and (12 -lt 10)
 (5 -lt 6) -or (12 -lt 10)
 
@@ -14,10 +16,10 @@
 "Pat" -eq "pat"
 "Pat" -ceq "pat"
 "Patrick" -like "pat"
-"Patrick" -like "*at*"  #like operator allows *
+"Patrick" -like "*pat*"  #like operator allows *
 
 #not
--not 5 -eq 5
+-not (5 -eq 5)
 -not (15 -lt 6) -or (2 -lt 10)
 -not ((15 -lt 6) -or (2 -lt 10))
 
@@ -34,7 +36,7 @@ Get-Process | Where-Object {($_.Name -eq "iexplore") -or ($_.Name -eq "chrome") 
 Get-Service | Where-Object -filter {$_.Status -eq "Running"} #-filter parameter is positional
 
 #If you only have a single comparison .....
-Get-Service | where status -ne "Running"
+Get-Service | Where-Object status -ne "Running"
 
 #What does the following code do?
 Get-process | where-object {$_.name -notlike "*powershell*"} | 
@@ -48,15 +50,19 @@ gci ~ -Recurse -File  | sort Length -Descending |   select -First 2 | select nam
 
 ## Homework
 #3 on Windows VM you could search for all files larger than 5MB in C:\Windows\Fonts
-gci C:\Windows\Fonts | Where-Object length -gt 5MB
+Get-ChildItem C:\Windows\Fonts | Where-Object length -gt 5MB
 
 #5 Get the files in the current directory where the LastWriteTime is in the last week. 
 #(Hint: (Get-Date).AddDays(-7) will give you the date from a week ago.) 
-gci ~ | Where-Object LastWriteTime -lt (Get-Date).AddDays(-7)
+New-Item -ItemType File -Path ~ -Name "NewFile1"
+New-Item -ItemType File -Path ~ -Name "NewFile2"
+
+(Get-Date)  -gt (Get-Date).AddDays(-7)   #figure out how to compare dates
+
+Get-ChildItem -Path ~ -Recurse -File | Where-Object LastWriteTime -gt (Get-Date).AddDays(-7)
 
 #7.   Display the 10 processes with the greatest amount of CPU time.  
 #The process objects have a property called CPU.  
 #You will need to use cmdlets like Get-Process &, Select-Object & Sort-Object# to accomplish this.
-gps | Sort-Object CPU -Descending | Select-Object -First 2
-
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 2
 
