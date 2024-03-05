dc0a16a99934b69b12b03d46bb21caca180a834b
federpmatc <61851250+federpmatc@users.noreply.github.com>
2024-03-05 14:12:13 -0500

    Update Chapter 23 Discussion (4th).ps1

--- Chapter 23 Discussion (4th).ps1
+++ Chapter 23 Discussion (4th).ps1
@@ -3,9 +3,17 @@
     sleep 1
 } -End {get-date}
 
-1..100 | ForEach-Object -ThrottleLimit 100 -Parallel   {Write-Host $_
-    sleep 1
+Measure-Command {
+1..10 | ForEach-Object -Process { Write-Host $_   
+    sleep 1  }  
 }
+
+#the default throttle limit is 5
+Measure-Command {
+1..100 | ForEach-Object -ThrottleLimit 100 -Parallel   {Write-Host $_
+    sleep 1}
+}
+
 $count = 1..10
 foreach ($number in $count) {
     $name = "Pat"+$number+".txt"
@@ -13,7 +21,7 @@ foreach ($number in $count) {
 }
 
 #lab question 1
-$files = gci 'C:\windows\Fonts'
+$files = gci -Path ~ -File
 $files | ForEach-Object -Parallel {
-    Write-Host $_.Length  $_.Name
+    Write-Host "$($_.Length)`t $($_.Name)"
 }
