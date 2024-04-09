27c5e0aa4ddc41a63c48b2ec314fe999b8272682
federpmatc <61851250+federpmatc@users.noreply.github.com>
2024-04-09 09:57:47 -0500

    Update Chapter 20 Lab Answers ().ps1

--- Chapter 20 Lab Answers ().ps1
+++ Chapter 20 Lab Answers ().ps1
@@ -8,7 +8,7 @@ The name of the computer to check.
 .Example
 PS C:\> c:\scripts\Get-PhysicalAdapters -computer SERVER01
 #>
-[cmdletbinding()]   #
+#[cmdletbinding()]   #Must be first line after comment based helped, adds advanced features
 Param (
 [Parameter(Mandatory=$True, HelpMessage="Enter a computername to query")]
 [alias('host')]
@@ -19,3 +19,17 @@ Get-CimInstance -class win32_networkadapter –computername $computername |
  Where-Object { $_.PhysicalAdapter } |
  Select-Object MACAddress,AdapterType,DeviceID,Name,Speed
 Write-Verbose "Script finished."
+
+## 
+<# Notes
+1.)  We added [cmdletbinding()]
+This adds several additional features.  Not needed for this example.  Book doesn't cover this.
+2.) Making parameters mandatory, creating aliases and the parameter is a string
++ [Parameter(Mandatory = $true)] - this decorator will prompt user for variable value
++ [HelpMessage="Enter a computername to query")] - provides a user prompt
++ [Alias()] - allows for parameter alias
++ Supports -Verbose switch.  Recall that the PowerShell supports various output streams.
+The Information stream is intended for messages to understand what a script is doing
+The Verbose stream is intended for troubleshooting.  Output is controlled with $VerbosePreference variable.
+Default is $VerbosePreference = 'SilentlyContinue'.  Output is enable with 'Continue'
+#>
