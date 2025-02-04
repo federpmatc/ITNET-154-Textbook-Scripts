22d2086419083d20fa6a0874c961260ca0f63916
federpe <federpe@gmail.com>
2025-02-04 14:44:53 -0600

    Update Chapter 05 and 06 Lecture (4th - PS Providers and Pipe).ps1

--- Chapter 05 and 06 Lecture (4th - PS Providers and Pipe).ps1
+++ Chapter 05 and 06 Lecture (4th - PS Providers and Pipe).ps1
@@ -1,3 +1,6 @@
+# PSDrive is a data store location that you can access like a file system
+#drive. PSDrives provide a consistent way to access different types of data stores, 
+#such as the file system, registry, environment variables, and more.
 Get-PSDrive
 
 #CmdLets to see and manipulate the data in a PSDrive include an ‘item’ noun)
@@ -5,29 +8,29 @@ Get-Item Alias:
 Get-Item /   #refers to the object
 #On macOS and Linux we don't have drive letters.  The entire FS is mapped to  /
 Get-ChildItem / #refers to the items within the object
-Get-ChildItem / #Let's see what is in it
 
 #Most items have properties, just like files and folders have properties
 Get-ItemProperty /Users  #Items have properties
 Get-ItemProperty /Users  | select *
 
-New-Item -ItemType Directory -Name labs
-New-item -ItemType File -Name labs\test.txt
+New-Item -ItemType Directory -Name labs -Path ~
+New-item -ItemType File -Name labs\test.txt -Path ~
 
 Set-Item -Path labs\test.txt -Value "hello"  #not supported on mac
-Add-Content -Path  ./labs/test.txt -Value "hello"
+Add-Content -Path  ~/labs/test.txt -Value "hello"
+Get-Content -Path ~/labs/test.txt
 
 Get-Item Env:/PSModulePath
 Get-Item Env:/PSModulePath | select *
 
-Get-ChildItem -Path ~/Downloads -Filter *.txt #seems to be the best
-Get-ChildItem -Path ~/Downloads/* -Include *.txt,*.pdf  #include supports multiple conditions
-Get-ChildItem -Path ~/Downloads/* -Exclude *.txt -Filter D*
+Get-ChildItem -Path ~/Downloads -Filter *.msi #seems to be the best
+Get-ChildItem -Path ~/Downloads/* -Include *.txt,*.pdf,*.msi  #include supports multiple conditions
+Get-ChildItem -Path ~/Downloads/* -Exclude *.msi  -Filter T*
 
 Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 get-item -Path  Advanced
 Set-ItemProperty -path Advanced -PSProperty DontPrettyPath -Value 1
-
+Get-ItemProperty -path Advanced -Name DontPrettyPath
 #The Power of using the | in PowerShell - TS
 Get-Service
 #If you wanted just the names to be displayed, it would be very difficult in many environments
@@ -44,25 +47,27 @@ Get-Service | Where-Object status -EQ "Running" |Select-Object "DisplayName" | M
 Get-Process | Sort-Object -Property CPU -Descending | #end with a pipe it will pickup next line
 Select-Object -First 3 | Export-Csv ~\process.csv   #property values become column headings
 
+get-aduser -Filter *
 #Create several domain admins first if necessary
 New-ADUser `
 -AccountPassword (ConvertTo-SecureString "Password01" -AsPlainText -Force) `
 -Name "Admin1" `
 -Enabled $true `
--Path "CN=Users, DC=ITNET-112, DC=pri" `
+-Path "CN=Users, DC=ITNET, DC=pri" `
 -SamAccountName Admin1 `
--UserPrincipalName ("Admin1@ITNET-112.pri")
+-UserPrincipalName ("Admin1@ITNET.pri")
 
 New-ADUser `
 -AccountPassword (ConvertTo-SecureString "Password01" -AsPlainText -Force) `
 -Name "Admin2" `
 -Enabled $true `
--Path "CN=Users, DC=ITNET-112, DC=pri" `
+-Path "CN=Users, DC=ITNET, DC=pri" `
 -SamAccountName Admin2 `
--UserPrincipalName ("Admin2@ITNET-112.pri")
+-UserPrincipalName ("Admin2@ITNET.pri")
 
 #Add Admin1 & Admin2 to Admin Groups
 Add-ADGroupMember -Identity 'Domain Admins' -Members 'Admin1','Admin2'
+Get-ADUser -Filter { Name -like "*Admin*" }
 
 Get-ADGroupMember -Identity "Domain Admins" | Set-ADUser -City "Milwaukee"
 
@@ -73,15 +78,15 @@ get-process note*
 get-process note* |Stop-Process -Confirm
 
 notepad;notepad
-get-process | Export-csV ~\processes.csv #we see way more data.  We could use select-object * to see all data
-Get-ADUser -Filter * | export-csv $env:userprofile\users.csv  #might be helpful to be able to make edits
+get-process | Export-csV ~\processes.csv -IncludeTypeInformation #we see way more data.  We could use select-object * to see all data
+Get-ADUser -Filter * | export-csv ~\users.csv  #might be helpful to be able to make edits
 
-notepad ~\processes.csv
+get-content ~\processes.csv
 #first line is the type of object
 #second line are properties
 #each row there after is a an object with it's properties
 
-Import-Csv ~\processes.csv | Where-Object name -like "note*"| Stop-Process 
+Import-Csv ~\processes.csv |gm # | Where-Object name -like "note*"| Stop-Process 
 
 #different format, same data as CSV.  
 #People prefer to use CliXML because it can handle more complex data
@@ -97,14 +102,10 @@ Get-ChildItem | Out-Host
 #pipe output to a file
 Get-ChildItem | Out-File ~\dir.txt
 
-$printer = Get-Printer |Out-GridView -Title "Select a printer" -OutputMode Single | Select-Object -ExpandProperty name
-Get-EventLog System  -Newest 5 -EntryType Error,Warning | Select-Object TimeGenerated, Source, Message | fl | Out-Printer -Name $printer
-
 #pipe to gridview
 #The Out-GridView cmdlet sends the output from a command to a grid view window where the output is displayed in an interactive table.
 #Because this cmdlet requires a user interface, it does not work on Server Core installations of Windows Server Fun to play with.  
 Get-ChildItem c:\ | Out-GridView
-Get-alias | out-gridview -PassThru
 
 #Export/Import vs get-content
 #get-content will not parse, import parses data and puts back into a format similar to original
@@ -112,3 +113,30 @@ Get-Process | Sort-Object -Property CPU -Descending| Select-Object -First 5 | Ex
 notepad gp.txt
 get-content gp.txt
 Import-Csv gp.txt
+
+######
+#this script will run when PowerShell starts
+$PROFILE
+
+#I want to add the following
+New-Item -ItemType Directory -path C:\Scripts -Force
+New-Item -ItemType File -path C:\Scripts -name DumbScript.ps1 
+Add-Content -path C:\Scripts\DumbScript.ps1 -value "Write-Host 'I am a dumb script'"   
+
+#About_environment_variables
+#$env:path specifies the path environment variable#The path environment variable is a list of directories that the operating system searches when you enter a command in the command prompt or PowerShell.
+$env:path 
+$env:Path -split ';'
+$env:path = $env:path + ';C:\Scripts'
+
+#about_profiles
+#The PowerShell profile is a script that runs every time you start PowerShell.
+
+New-Item -ItemType File -path $PROFILE -Force
+Add-Content -path $PROFILE -value '$env:path = $env:path + ";C:\Scripts"'
+Add-Content -path $PROFILE -value 'write-host "Welcome to Pats PowerShell!"'
+
+Get-Content $PROFILE
+notepad.exe $PROFILE
+
+
