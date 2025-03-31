bcd65354793648063c9aa0cf50a9af632aac60d1
federpmatc <61851250+federpmatc@users.noreply.github.com>
2025-03-31 12:59:45 -0500

    Update Chapter 20-pre.ps1

--- Chapter 20-pre.ps1
+++ Chapter 20-pre.ps1
@@ -1,4 +1,3 @@
-#This is a comment
 <#
 .SYNOPSIS
 Get-DiskInventory retrieves logical disk information from one or
@@ -6,7 +5,7 @@ more computers.
 .DESCRIPTION
 Get-DiskInventory uses CIM to retrieve the Win32_LogicalDisk
 instances from one or more computers. It displays each disk's
-drive letter, free space, total size, and percentage of free
+drive let ter, free space, total size, and percentage of free
 space.
 .PARAMETER computername
 The computer name
@@ -15,17 +14,18 @@ The drive type to query. See Win32_LogicalDisk documentation
 for values. 3 is a fixed disk, and is the default.
 .EXAMPLE
 Get-DiskInventory -ComputerName Server22-02 -drivetype 3
+.EXAMPLE
+Get-DiskInventory -ComputerName Win11-Client -drivetype 3
 #>
 param (
-$computername,
-$drivetype
+[string[]]$computername,
+[int]$drivetype
 )
 
-Get-CimInstance -class Win32_LogicalDisk  -ComputerName $computername
- -filter "drivetype=$drivetype" |
+Get-CimInstance -class Win32_LogicalDisk  -ComputerName $computername -filter "drivetype=$drivetype" |
  Sort-Object -property DeviceID |
- Select-Object -property DeviceID,
- @{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB}},
+ Select-Object -property DeviceID,@{label='FreeSpace(GB)';expression={$_.FreeSpace / 1GB}},
  @{label='Size(GB)';expression={$_.Size / 1GB}},
- @{label='%Free';expression={$_.FreeSpace / $_.Size * 100}}
-
+ @{label='%Free';expression={$_.FreeSpace / $_.Size * 100}},
+ @{label='System Name';expression={$_.SystemName}}
+ 
