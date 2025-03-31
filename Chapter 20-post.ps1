e038058db18144381aaa0c8d401b9a1ab1a24910
federpmatc <61851250+federpmatc@users.noreply.github.com>
2025-03-31 12:58:09 -0500

    Update Chapter 20-post.ps1

--- Chapter 20-post.ps1
+++ Chapter 20-post.ps1
@@ -1,43 +1,43 @@
-#This is a comment
-<#
-.SYNOPSIS
-Get-DiskInventory retrieves logical disk information from one or
-more computers.
-.DESCRIPTION
-Get-DiskInventory uses CIM to retrieve the Win32_LogicalDisk
-instances from one or more computers. It displays each disk's
-drive letter, free space, total size, and percentage of free
-space.
-.PARAMETER computername
-The computer name
-.PARAMETER drivetype
-The drive type to query. See Win32_LogicalDisk documentation
-for values. 3 is a fixed disk, and is the default.
-.EXAMPLE
-Get-DiskInventory -ComputerName Server22-02 -drivetype 3
-#>
-[CmdletBinding()]
-<#The Cmdlet Binding directive adds new features to our script
-we can use decorators to help manage our parameters
-we can use write-verbose within the script
-#>
-param (
-[Parameter(Mandatory=$True, HelpMessage="Enter a computer name!!!")]
-[Alias('host')]
-[string[]]$computername,
+#Hash tables consist of key/value pairs
+#https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-hashtable?view=powershell-7.5
+$number = 1
+$number 
 
-[ValidateSet(2,3)]
-[int]$drivetype = 3
-)
-Write-Verbose "Connecting to $computername"
-Write-Verbose "Looking for drive type $drivetype"
-<#
-Get-CimInstance -class Win32_LogicalDisk -ComputerName $computername `
- -filter "drivetype=$drivetype" |
- Sort-Object -property DeviceID |
- Select-Object -property DeviceID,
- @{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
- @{label='Size(GB)';expression={$_.Size / 1GB -as [int]}},
- @{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
-#>
-Write-host "All done!!!!!"
+$Class = "ITNET-154"
+$Class | Get-Member
+
+$Classes = "ITNET-154","ITNET-198", "ITNETN-199"
+$Classes
+$classes[1]
+
+#Key/Value pairs
+$name = @{"FullName"="Pat Feder";"ID" = 123456; age = 57}
+
+$name
+$name.keys
+
+$name.FullName
+$name['FullName']
+$name.age
+
+$names = @{"FullName"="Pat Feder"; "ID" = 123456; age = 55},
+@{"FullName"="Jeanene Feder"; "ID" = 789; age = 54},
+@{"FullName"="Yamaha Feder"; "ID" = "abcdef"; age = 30}
+
+$names
+
+$example = @{
+    label='Size(MB)'
+    expression={$_.Length / 1MB -as [int]}
+}
+$example
+
+Get-ChildItem ~ -Recurse | sort-object -Property Length -Descending | Select-Object -First 2| 
+Select-Object Name, Length
+
+Get-ChildItem ~ -Recurse | sort-object -Property Length -Descending | Select-Object -First 4 |
+ Select-Object Name, Length | ForEach-Object {
+    Write-Host "Pat Feder says the FileName is: ", $_.Name}
+
+Get-ChildItem ~ -Recurse | sort-object -Property Length -Descending | Select-Object -First 2| 
+Select-Object -Property Name , @{label='Size(MB)';expression={($_.Length / 1MB)}}
