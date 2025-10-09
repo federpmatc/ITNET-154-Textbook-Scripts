0041e85eb1ff571eb4a3da1405b773a1858ec3b9
federpmatc <61851250+federpmatc@users.noreply.github.com>
2025-10-09 09:48:28 -0500

    Update Chapter 07 Lab Answers (4th - Adding Commands).ps1

--- Chapter 07 Lab Answers (4th - Adding Commands).ps1
+++ Chapter 07 Lab Answers (4th - Adding Commands).ps1
@@ -1,20 +1,48 @@
-Install-Module ????
+#Q1
+find-module *wordle*
+Install-Module PSWordle
+Get-InstalledModule
 
-Get-Command –module ????
 
-Find-Module -Command Compress-Archive | Install-Module -Force
+#Q2
+Get-Command –module PSWordle
 
-Import-Module Microsoft.PowerShell.Archive
 
-1..10 #will create a collection of the numbers between 1 and 10. 
+#Q3
+get-command Compress-Archive
+get-module  Microsoft.PowerShell.Archive #Gets module
+find-module  Microsoft.PowerShell.Archive #Most likely same version, if not complete the command below
+Install-Module  Microsoft.PowerShell.Archive -Force #installs module from PSGallery
+Find-Module -Command Compress-Archive | Install-Module -Force #Another way to install the module
 
-New-Item ~/TestFolder -ItemType Directory
-1..10 | ForEach-Object {New-Item "~/TestFolder/$_.txt" -ItemType File -Value $_}
 
+#Q4
+import-module PSWordle
+get-module
+
+
+#Q5
+1..10 #will create a collection of the numbers between 1 and 10.
+New-Item ~/TestFolder -ItemType Directory #New folder
+1..10 | ForEach-Object {New-Item "~/TestFolder/$_.txt" -ItemType File -Value $_} #Create 10 files
+
+
+#Q6
 Compress-Archive ~/TestFolder/* -DestinationPath ~/TestFolder.zip
+Get-ChildItem ~
+
 
 Expand-Archive ~/TestFolder.zip -DestinationPath ~/TestFolder2
+Get-ChildItem ~
+Get-ChildItem ~/TestFolder2
 
-$reference = Get-ChildItem ~/TestFolder| Select-Object -ExpandProperty name
-$difference = Get-ChildItem ~/TestFolder3| Select-Object -ExpandProperty name
+
+#Q7
+New-Item -ItemType File -Name ExtraFile.txt -Path ~/TestFolder2 -Force
+
+
+#Q8
+$reference = Get-ChildItem ~/TestFolder | Select-Object -ExpandProperty name
+$difference = Get-ChildItem ~/TestFolder2 | Select-Object -ExpandProperty name
 Compare-Object -ReferenceObject $reference -DifferenceObject $difference
+
