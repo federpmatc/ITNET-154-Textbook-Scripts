409c5f1e03033b2b51b2a377e831856108397456
federpmatc <61851250+federpmatc@users.noreply.github.com>
2025-10-21 11:16:03 -0500

    Refactor PowerShell commands in lab answers

--- Chapter 12 and 13 Lab Answers.ps1
+++ Chapter 12 and 13 Lab Answers.ps1
@@ -1,12 +1,26 @@
 #Chapter 12
 Get-Command -Module PSReadLine
+
+#Which is better??
 Get-Command -Module PSReadLine -Verb get
+Get-Command -Module PSReadLine | Where-Object name -like get*
+
+#The 2nd line below contains a shortcut way of using where-object
 Get-ChildItem -File -Path C:\Windows\Fonts | Where-Object {$_.length -ge 5MB}
+Get-ChildItem -File -Path C:\Windows\Fonts | Where length -ge 5MB
+
 Find-Module -name PS* |Where-Object {$_.author -like "Micro*"}
+
 Get-ChildItem -Path ~ -file -Recurse| Where-Object {$_.LastWriteTime -ge (Get-Date).AddDays(-7)}
+
+#The following two lines are equivalent
 Get-Process -Name 'P*', 'b*'
-Get-Process | Sort-Object -Property CPU |Select-Object -First 10 |Measure-Object -Property CPU
-Get-Process | Where-Object {$_.Name -notlike 'W*'} |Sort-Object -Property CPU |Select-Object -First 10 |Measure-Object -Property CPU
+Get-Process | Where-Object {($_.name -like "p*") -or ($_.name -like "b*")}
+
+Get-Process | Sort-Object -Property CPU -Descending |Select-Object -First 10
+
+Get-Process | Sort-Object -Property CPU -Descending |Select-Object -First 10 |Measure-Object -Property CPU -Average
+Get-Process | Where-Object {$_.Name -notlike 'W*'} |Sort-Object -Property CPU -Descending |Select-Object -First 10 |Measure-Object -Property CPU -Average
 Get-Process -Name 'svchost'
 
 #Chapter 13
@@ -19,10 +33,12 @@ Exit-PSSession
 #SSH needs to be on the local machine and the remote system needs to have PowerShell SSH installed
 Enter-PSSession -HostName Server22-01
 
+#this will return desrialized objects (properties, no methods)
 Invoke-command -ComputerName Server22-01 -ScriptBlock {
     Get-Service | where-object status -eq stopped
 }
 
+#create a variable that contains a script block
 $commands = {
     Get-Service | where-object status -eq stopped
 }
@@ -41,4 +57,3 @@ Invoke-command -ComputerName (Get-Content -Path ~\computers.txt) -ScriptBlock $c
 
 $commands = {$PSVersionTable}
 Invoke-command -ComputerName (Get-Content -Path ~\computers.txt) -ScriptBlock $commands
-
